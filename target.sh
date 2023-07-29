f49a3a97858d42698d518a18e74cd6a016a3e353
Bytehxz <caballero.daniel@proton.me>
2023-07-29 12:08:55 -0600

    update; target.sh

--- target.sh
+++ target.sh
@@ -7,7 +7,7 @@ machine_name=$(/bin/cat /home/bytehxz/Documents/Entorno/victima | awk '{print $2
 
 if [ $ip_address ] && [ $machine_name ]; then
     iconT="󰯐 "
-    echo "<txt><span weight='Bold' fgcolor='Red'><i>$iconT</i></span><span weight='Bold' fgcolor='Cyan'><i>$ip_address</i></span> - <span weight='Bold' fgcolor='Gray'><i>$machine_name</i></span></txt>"
+    echo "<txt><span weight='Bold' fgcolor='Red'><i>$iconT</i></span><span weight='Bold' fgcolor='Cyan'><i>$ip_address</i></span> - <span weight='Bold' fgcolor='LightGray'><i>$machine_name</i></span></txt>"
 else
     noTar="󰈉 "
     Ntarget="No target"
