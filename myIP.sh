ce332f057f2ae887c2c2a49a85f9f4ef41cd32b2
Bytehxz <juandaniel.caballerohernandez@gmail.com>
2023-08-03 23:08:43 -0600

    myIP - noIP

--- myIP.sh
+++ myIP.sh
@@ -3,6 +3,8 @@
 IP=$(/usr/sbin/ifconfig eth0 | grep "netmask" | awk '{print $2}')
 Msg=" "
 
-
-echo "<txt><span weight='Bold' fgcolor='Cyan'><i>$Msg</i></span><span weight='Bold' fgcolor='LightGreen'><i>$IP</i></span></txt>"
-
+if [ $IP ]; then
+  echo "<txt><span weight='Bold' fgcolor='Cyan'><i>$Msg</i></span><span weight='Bold' fgcolor='LightGreen'><i>$IP</i></span></txt>"
+else
+  echo "<txt><span weight='Bold' fgcolor='Cyan'><i>$Msg</i></span><span weight='Bold' fgcolor='LightGreen'><i>No IP</i></span></txt>"
+fi
