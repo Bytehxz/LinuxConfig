e9ca0d0d3b1ee6d9476c2923f89fe188047e0387
Bytehxz <bytehxz@bytehxz.com>
2023-11-02 22:47:51 -0600

    plugin changes history

--- Neovim/custom/plugins.lua
+++ Neovim/custom/plugins.lua
@@ -16,6 +16,17 @@ local plugins = {
     end,
   },
 
-
+  -- Para ver el historial de cambios de un archivo, tipo git pero de un archivo
+  {
+    "jiaoshijie/undotree",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = true,
+    --config = function ()
+    --  require "custom.configs.undotree-config"
+    --end,
+    keys = { -- load the plugin only when using it's keybinding:
+      { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>", "Undotree file"  },
+    },
+  }
 }
 return plugins
