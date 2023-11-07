2020f2ab7a0892b02fa0b1301c7cb126467c024d
Bytehxz <bytehxz@bytehxz.com>
2023-11-06 18:11:30 -0600

    clangd plugin nvim

--- Neovim/custom/configs/lspconfig.lua
+++ Neovim/custom/configs/lspconfig.lua
@@ -10,3 +10,13 @@ lspconfig.pyright.setup({
   capabilities = capabilities,
   filetypes = {"python"},
 })
+
+lspconfig.clangd.setup({
+  on_attach = function (client, bufnr)
+    client.server_capabilites.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
+  capabilities = capabilities,
+  -- filetypes = {"python"},
+})
+
